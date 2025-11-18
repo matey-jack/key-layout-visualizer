43f4b69e5ffbfac0bd96ad1d4bba5f79a4a799e6
Robert Will <matey.jack@gmail.com>
2025-11-19 00:18:01 +0100

    remove old Ergoplank with only 1.25u keys in bottom row.

---
+++ AGENTS.md
@@ -0,0 +1,2 @@
+
+Run tests using `npm test`.
--- src/layout/ergoPlank60LayoutModel.ts
+++ src/layout/ergoPlank60LayoutModel.ts
@@ -1,11 +1,7 @@
 import {FlexMapping, KEY_COLOR, KeyboardRows, RowBasedLayoutModel} from "../base-model.ts";
 import {defaultKeyColor} from "./layout-functions.ts";
 
-/*
-    This version with uniform 1.25u keys in the bottom row is not actually shown in the app.
-    See below for the actual ergoPlank60.
- */
-export const ergoPlankRegularLayoutModel: RowBasedLayoutModel = {
+export const ergoPlank60LayoutModel: RowBasedLayoutModel = {
     name: "ErgoPlank 60",
     description: `"The most ergonomic key layout that fits into a standard "60%" keyboard case."
     Hand distance is maximized. Row stagger is equal to a "cleave-style ergonomic" keyboard.
@@ -15,13 +11,13 @@ export const ergoPlankRegularLayoutModel: RowBasedLayoutModel = {
     Biggest difference to the Katana is that the Shift keys are closer to the Pinky home position.
     This is achieved by making the Shift keys bigger and the home row edge key minimally small, i.e. 1u.`,
 
-    // row lengths: 14, 14 (but +1 gap!), 15, 14, 12
+    // row lengths: 14, 14 (but +1 gap!), 15, 14, 11
     thirtyKeyMapping: [
         ["Esc", "1", "2", "3", "4", "5", "[", "]", "6", "7", "8", "9", "0", "⌫"],
         ["↹", 0, 1, 2, 3, 4, "-", null, "=", 5, 6, 7, 8, 9, "\\"],
         ["⌦", 0, 1, 2, 3, 4, "⇤", "`~", "⇥", 5, 6, 7, 8, 9, "'"],
         ["⇧", 0, 1, 2, 3, 4, "⇞", "⇟", 9, 5, 6, 7, 8, "⇧"],
-        ["Ctrl", "Cmd", "", "Alt", "Fn", "⏎", "⍽", "Fn", "AltGr", "Menu", "Cmd", "Ctrl"],
+        ["Ctrl", "Cmd", "", "Alt", "⏎", "Fn", "⍽", "AltGr", "Menu", "Cmd", "Ctrl"],
     ],
 
     thumb30KeyMapping: [
@@ -29,7 +25,7 @@ export const ergoPlankRegularLayoutModel: RowBasedLayoutModel = {
         ["↹", 0, 1, 2, 3, 4, "=", null, "`~", 5, 6, 7, 8, 9, "⏎"],
         ["⌦", 0, 1, 2, 3, 4, "⇤", "\\", "⇥", 5, 6, 7, 8, 9, "'"],
         ["⇧", 0, 1, 2, 3, 4, "⇞", "⇟", "/", 5, 6, 7, 8, "⇧"],
-        ["Ctrl", "Cmd", "", "Alt", "Fn", 0, "⍽", "Fn", "AltGr", "Menu", "Cmd", "Ctrl"],
+        ["Ctrl", "Cmd", "", "Alt", 0, "Fn", "⍽", "AltGr", "Menu", "Cmd", "Ctrl"],
     ],
 
     // todo
@@ -42,7 +38,7 @@ export const ergoPlankRegularLayoutModel: RowBasedLayoutModel = {
         [1, 0, 1, 2, 3, 3, 3, null, 6, 6, 6, 7, 8, 9, 9],
         [0, 0, 1, 2, 3, 3, 3, 6, 6, 6, 6, 7, 8, 9, 9],
         [0, 1, 2, 3, 3, 3, 3, 6, 6, 6, 6, 7, 8, 9],
-        [0, 1, 2, 4, 4, 4, 5, 5, 5, 7, 8, 9],
+        [0, 1, 2, 4, 4, 5, 5, 5, 7, 8, 9],
     ],
 
     hasAltFinger: (row: number, col: number) =>
@@ -54,7 +50,7 @@ export const ergoPlankRegularLayoutModel: RowBasedLayoutModel = {
         [2.0, 2.0, 1.0, 1.0, 1.5, 2.0, 3.0, 3.0, 3.0, 2.0, 1.5, 1.0, 1.0, 2.0, 2.0],
         [1.5, 0.2, 0.2, 0.2, 0.2, 2.0, 3.0, 3.0, 3.0, 2.0, 0.2, 0.2, 0.2, 0.2, 1.5],
         [1.0, 1.5, 1.5, 1.0, 2.0, 3.0, 3.0, 3.0, 3.0, 2.0, 1.0, 1.5, 1.5, 1.0],
-        [3.0, 3.0, 2.0, 1.5, 0.2, 1.0, 1.0, 0.2, 1.5, 2.0, 3.0, 3.0],
+        [3.0, 3.0, 2.0, 1.5, 0.2, 1.5, 0.2, 1.5, 2.0, 3.0, 3.0],
     ],
 
     rowStart: (row: KeyboardRows) => row >= KeyboardRows.Lower ? 0.25 : 0,
@@ -62,22 +58,26 @@ export const ergoPlankRegularLayoutModel: RowBasedLayoutModel = {
     keyWidth: (row: KeyboardRows, col: number): number => {
         const numCols = ergoPlank60LayoutModel.thirtyKeyMapping![row].length;
         if (row == KeyboardRows.Bottom) {
-            // using 12 equal 1.25u keys is pretty, but the central ones are hard to reach.
-            // we could use 10 equal 1.5u keys,
-            // that would still be enough keys, but not sure if that improves the center enough.
-            // also I think it's too big for modifiers under the palm.
-            return 1.25;
+            // Having one central key serves the purpose of bringing its neighbors into the "hands-fixed" range of the thumbs.
+            // I saw this not only on the Katana60, but also some conventional keyboards with a split space bar.
+            // The big question is: can we size and position thumb keys such that each thumb can comfortably hit two of them
+            // without moving the hand?
+            // The typical use would be one per thumb as Space or a letter, the other as a modifier or layer key.
+            switch (col) {
+                case 4:
+                case 6:
+                    return 1.5;
+                default:
+                    return 1.25;
+            }
         }
-        const widthOfEdgeKey = [1.5, 1.25, 1, 1.25]
+        const widthOfEdgeKey = [1.5, 1.25, 1, 1.25];
         if (col == 0 || col == numCols - 1) {
             return widthOfEdgeKey[row];
         }
-        if (row == KeyboardRows.Upper && (col == 7)) {
+        if (row == KeyboardRows.Upper && col == 7) {
             return 0.5;
         }
-        // if (row == KeyboardRows.Lower && (col == 6)) {
-        //     return 1.5;
-        // }
         return 1;
     },
 
@@ -88,56 +88,9 @@ export const ergoPlankRegularLayoutModel: RowBasedLayoutModel = {
     symmetricStagger: true,
 
     getSpecificMapping: (_: FlexMapping) => undefined,
-}
-
-export const ergoPlank60LayoutModel: RowBasedLayoutModel = {
-    ...ergoPlankRegularLayoutModel,
-    thirtyKeyMapping: replaceLast(ergoPlankRegularLayoutModel.thirtyKeyMapping!,
-        ["Ctrl", "Cmd", "", "Alt", "⏎", "Fn", "⍽", "AltGr", "Menu", "Cmd", "Ctrl"]
-    ),
-    thumb30KeyMapping: replaceLast(ergoPlankRegularLayoutModel.thumb30KeyMapping!,
-        ["Ctrl", "Cmd", "", "Alt", 0, "Fn", "⍽", "AltGr", "Menu", "Cmd", "Ctrl"]
-    ),
-    // fullMapping: replaceLast(ergoPlankRegularLayoutModel.fullMapping, []),
-    singleKeyEffort: replaceLast(ergoPlankRegularLayoutModel.singleKeyEffort,
-        [3.0, 3.0, 2.0, 1.5, 0.2, 1.5, 0.2, 1.5, 2.0, 3.0, 3.0]
-    ),
-    mainFingerAssignment: replaceLast(ergoPlankRegularLayoutModel.mainFingerAssignment,
-        [0, 1, 2, 4, 4, 5, 5, 5, 7, 8, 9]
-    ),
-    keyWidth: (row: KeyboardRows, col: number): number => {
-        if (row == KeyboardRows.Bottom) {
-            // Having one central key serves the purpose of bringing its neighbors into the "hands-fixed" range of the thumbs.
-            // I saw this not only on the Katana60, but also some conventional keyboards with a split space bar.
-            // The big question is: can we size and position thumb keys such that each thumb can comfortably hit two of them
-            // without moving the hand?
-            // The typical use would be one per thumb as Space or a letter, the other as a modifier or layer key.
-            switch (col) {
-                case 4:
-                case 6:
-                    // todo: make it a standard 1.5 and spread the wasted 0.25u evenly across all 10 bottom row gaps.
-                    // this is going to need another change in the data model.
-                    return 1.5; // + 0.125;
-                case 5:
-                    return 1.25;
-            }
-        }
-        return ergoPlankRegularLayoutModel.keyWidth(row, col);
-    },
-
-    // keyCapWidth(row: KeyboardRows, col: number): number {
-    //     // We have this entire concept of keyCapWidth only for the Escape key.
-    //     // But, let's see: maybe it will be useful elsewhere later.
-    //     if (row == KeyboardRows.Number && col == 0) return 1;
-    //     return this.keyWidth(row, col);
-    // },
 
     keyColorClass(label: string, row: KeyboardRows, col: number) {
         if (label == "⏎" || label == "Esc") return KEY_COLOR.HIGHLIGHT;
         return defaultKeyColor(label, row, col);
     },
-}
-
-function replaceLast<T>(list: T[], last: T) {
-    return [...list.slice(0, -1), last];
 }
