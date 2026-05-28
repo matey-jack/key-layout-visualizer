90e08e451d8cf776957159a6aa067b10f874cdaa
Robert Will <matey.jack@gmail.com>
2026-05-28 20:53:40 +0200

    Use Insert key consistently in Thumby frame mappings.

--- src/layout/eb65MidshiftNarrowLayoutModels.ts
+++ src/layout/eb65MidshiftNarrowLayoutModels.ts
@@ -61,7 +61,7 @@ export const eb65MidshiftRightRetLayoutModel: LayoutModel = {
             ["↹", 0, 1, 2, 3, 4, "⌦", 5, 6, 7, 8, 9, "'", "⌫"],
             ["⇧", 0, 1, 2, 3, 4, "=", "\\", 5, 6, 7, 8, 9, "⇧", "⏎"],
             ["Ctrl", 0, 1, 2, 3, 4, "⇤", null, "⇥", 5, 6, 7, 8, "/", null, "↑", null],
-            [null, "Cmd", "Fn", "", "Alt", 0, "⍽", "AltGr", "Menu", "Ctrl", "←", "↓", "→"],
+            [null, "Cmd", "Fn", "Ins", "Alt", 0, "⍽", "AltGr", "Menu", "Ctrl", "←", "↓", "→"],
         ],
     },
 
