32169a1c301a69013e56ce0c51ddaf899e992db6
Robert Will <matey.jack@gmail.com>
2026-05-30 17:26:55 +0200

    remove Posthog from dev environments that don't have it's key set.

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import { render } from 'preact'
 import posthog from 'posthog-js'
 import { App } from './app.tsx'
 
-posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
-    api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-});
+if (import.meta.env.VITE_PUBLIC_POSTHOG_KEY) {
+    posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+    });
+}
 
 render(<App />, document.getElementById('app')!);
 
