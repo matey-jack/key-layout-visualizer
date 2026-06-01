5f57db6109dfb072126913b0bcc85d88ba55e1d3
Robert Will <matey.jack@gmail.com>
2026-06-01 17:56:05 +0200

    disable Sentry on local.

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,32 @@ import { defineConfig } from 'vitest/config';
 import preact from '@preact/preset-vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-    base: '/key-layout-visualizer/',
-    plugins: [preact()],
-    build: {
-        outDir: 'dist',
-    },
-    server: {
-        port: 3000,
-    },
-    test: {
-        environment: 'jsdom',
-        globals: true,
-        setupFiles: './src/test/setup.ts',
-        exclude: ['e2e/**', 'node_modules/**']
-    }
-})
+export default defineConfig(({ mode }) => {
+    return {
+        base: '/key-layout-visualizer/',
+        plugins: [
+            preact(),
+            {
+                name: 'conditional-sentry',
+                transformIndexHtml(html) {
+                    if (mode !== 'production') {
+                        return html.replace(/<script\b[^>]*src="https:\/\/js-de\.sentry-cdn\.com\/[^>]*><\/script>/gi, '');
+                    }
+                    return html;
+                }
+            }
+        ],
+        build: {
+            outDir: 'dist',
+        },
+        server: {
+            port: 3000,
+        },
+        test: {
+            environment: 'jsdom',
+            globals: true,
+            setupFiles: './src/test/setup.ts',
+            exclude: ['e2e/**', 'node_modules/**']
+        }
+    };
+});
