9c4ca10a3d6e77b20272db5eebfed19323b81919
Robert Will <matey.jack@gmail.com>
2026-06-03 13:51:06 +0200

    KLE export now with colors, only they are weird.

--- src/kle-export.ts
+++ src/kle-export.ts
@@ -1,10 +1,25 @@
 import {type KleKey, type KleKeyboardMetadata, KleKeyboard, serialize} from '@kcf-hub/kle-serial';
 import type {FlexMapping, LayoutModel} from './base-model.ts';
 import  {isSplit, type LayoutOptions} from './app-model.ts';
-import {fillMapping, getKeyPositions} from './layout/layout-functions.ts';
+import {fillMapping, getKeyPositions, isHomeKey, defaultKeyColor} from './layout/layout-functions.ts';
 import {ergoboardCentralLayoutModel} from './layout/ergoboardCentralLayoutModel.ts';
 import {colemakMapping} from './mapping/colemakMappings.ts';
 import { writeFileSync } from 'node:fs';
+
+const colorClassMap: Record<string, string> = {
+    "home-key": "#c3f3ac",
+    "edge-key": "#729bac",
+    "highlighted-key": "#c6b07c",
+    "unlabeled": "#e7e7e7",
+    "pinky": "#BCA7D4",
+    "ringy": "#A3BFD9",
+    "middy": "#a1c67e",
+    "lindex": "#DDD88C",
+    "rindex": "#C6B4A5",
+    "lthumb": "#D7A86E",
+    "rthumb": "#D8A39D",
+    "": "#faf5eb",
+};
 
 const basicMetadata: KleKeyboardMetadata = {
     author: "Robert Jack Will",
@@ -22,43 +37,50 @@ export function buildKle(layout: LayoutModel, mapping: FlexMapping, options: Par
     const charMap = fillMapping(layout, mapping);
     const positions = getKeyPositions(layout, isSplit(options), charMap!);
     const leftEdge = Math.min(...positions.map( p => p.colPos));
-    const keys: KleKey[] = positions.map( (pos) => ({
-        x: pos.colPos - leftEdge,
-        y: pos.row,
-        width: pos.keyCapWidth,
-        // TODO: get height from wherever it's stored
-        height: 1,
-        labels: [
-            // it's a 3×3 matrix, but we fill only enough to reach the center
-            "", "", "",
-            "", pos.label
-        ],
-        // TODO: get from layout.keyColorClass plus the CSS
-        color: "",
-        textColor: [],
-        textSize: [],
-        default: {
-            textColor: "",
-            textSize: 0,
-        },
-        x2: 0,
-        y2: 0,
-        width2: 0,
-        height2: 0,
-        rotation_x: 0,
-        rotation_y: 0,
-        rotation_angle: 0,
-        decal: false,
-        ghost: false,
-        stepped: false,
-        nub: false,
-        profile: "",
-        sm: "",
-        sb: "",
-        st: "",
-        f2: undefined,
-        align: undefined,
-    }))
+    const keys: KleKey[] = positions.map( (pos) => {
+        const colorFn = layout.keyColorClass || defaultKeyColor;
+        const colorClass = isHomeKey(layout, pos.row, pos.col)
+            ? "home-key"
+            : (colorFn(pos.label, pos.row, pos.col) || (!pos.label ? "unlabeled" : ""));
+        const color = colorClassMap[colorClass] || colorClassMap[""];
+
+        return {
+            x: pos.colPos - leftEdge,
+            y: pos.row,
+            width: pos.keyCapWidth,
+            // TODO: get height from wherever it's stored
+            height: 1,
+            labels: [
+                // it's a 3×3 matrix, but we fill only enough to reach the center
+                "", "", "",
+                "", pos.label
+            ],
+            color: color,
+            textColor: [],
+            textSize: [],
+            default: {
+                textColor: "",
+                textSize: 0,
+            },
+            x2: 0,
+            y2: 0,
+            width2: 0,
+            height2: 0,
+            rotation_x: 0,
+            rotation_y: 0,
+            rotation_angle: 0,
+            decal: false,
+            ghost: false,
+            stepped: false,
+            nub: false,
+            profile: "",
+            sm: "",
+            sb: "",
+            st: "",
+            f2: undefined,
+            align: undefined,
+        };
+    })
     const keyboard = new KleKeyboard();
     keyboard.meta = basicMetadata;
     keyboard.keys = keys;
